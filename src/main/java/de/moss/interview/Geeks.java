96c47fd8d3b2f83af42d6c4a6f2d33abe39f6d8e
Aditya Vibhute <adityavibhute@Adityas-Air.fritz.box>
2025-07-25 07:05:17 +0200

    Aditya Interview

---
+++ src/main/java/de/moss/interview/Geeks.java
@@ -0,0 +1,12 @@
+package de.moss.interview;
+
+public class Geeks {
+    public static void main(String args[])
+    {
+        System.out.println("Hellos Worlds!");
+    }
+    public void call()
+    {
+        System.out.println("Calling");
+    }
+}
--- src/main/java/de/nufin/interviews/Application.java
+++ src/main/java/de/nufin/interviews/Application.java
@@ -1,13 +1,17 @@
 package de.nufin.interviews;
 
+import de.moss.interview.Geeks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        Geeks g1 = new Geeks();
+        g1.call();
     }
 
 }
